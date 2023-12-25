6cd23f1b47f93ee948cbf67411bb34b7e4658ce4
Smith <samspike46@gmail.com>
2023-12-25 04:04:39 +0100

    Calculator JS optimized

--- calculator/app.js
+++ calculator/app.js
@@ -1,50 +1,44 @@
-let displayInput = document.querySelector(".display .input"),
-    displayOutput = document.querySelector(".display .output"),
-    keys = document.querySelectorAll(".key"),
-    keypad = document.querySelector(".keypad"),
-    op = document.querySelectorAll(".op");
-
+let keypad = document.querySelector(".keypad");
 keypad.addEventListener("click", ({ target }) => {
-    if(target.classList.contains("keypad")) return 0;
+    if (target.classList.contains("keypad")) return 0;
     let displayInput = document.querySelector(".display .input"), displayOutput = document.querySelector(".display .output");
     if (Boolean(Object.keys(target.dataset).length)) {
         // An operator 
-        let num_precision = (val)=>{
-            try{
-                if(Number.isInteger(val)){
+        let num_precision = (val) => {
+            try {
+                if (Number.isInteger(val)) {
                     return val;
-                }else if(!Number.isNaN(val)) {
+                } else if (!Number.isNaN(val)) {
                     return parseFloat(val.toFixed(8));
                 }
-            }catch(e){
-                return "NaN";  
+            } catch (e) {
+                return "NaN";
             }
         }
         switch (target.dataset.key) {
             case "/":
-                displayInput.textContent = displayInput.textContent +"/";
+                displayInput.textContent = displayInput.textContent + "/";
                 return 0;
             case "*":
             case "+":
             case "-":
-                displayInput.textContent = displayInput.textContent +`${target.dataset.key}`;
+                displayInput.textContent = displayInput.textContent + `${target.dataset.key}`;
                 return 0;
             case "=":
-                try{
+                try {
                     let val = eval(displayInput.textContent);
                     displayOutput.textContent = num_precision(val);
-                }catch(e){
+                } catch (e) {
                     displayOutput.textContent = "NaN";
-
                 }
                 return 0;
             case "%":
-                try{
+                try {
                     let i = eval(displayInput.textContent);
                     i = i / 100;
                     displayInput.textContent = num_precision(i);
                     displayOutput.textContent = num_precision(i);
-                }catch (e){
+                } catch (e) {
                     displayOutput.textContent = "NaN";
                 }
                 return 0;
@@ -53,15 +47,12 @@ keypad.addEventListener("click", ({ target }) => {
                 displayOutput.textContent = 0;
                 return 0;
             case "backspace":
-                if (displayInput.textContent.length == 1) {
-                    displayInput.textContent = 0;
-                } else {
-                    displayInput.textContent = displayInput.textContent.slice(0, displayInput.textContent.length - 1);
-                }
+                displayInput.textContent = (displayInput.textContent.length == 1) ? 0 :
+                    displayInput.textContent.slice(0, displayInput.textContent.length - 1);
                 return 0;
 
 
         }
-    } 
+    }
     displayInput.textContent = displayInput.textContent == 0 ? target.textContent : displayInput.textContent + "" + target.textContent;
 })
